2ddf92193e67450bcca5ca41165d9b6d3fa05b57
Frithjof Thorvik <frithjot@stud.ntnu.no>
2021-10-31 01:15:13 +0200

    Deployment worked, trying to use refactored method

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,8 @@
 import express from "express";
 import socketIO from "socket.io";
 import http from "http";
-import db from "./models";
-// import { runExpress } from "./servers/express";
-// import { runSocket } from "./servers/socket";
-import path from "path";
-import cors from "cors";
-
-import UserRouter from "./routes/user";
+import { runExpress } from "./servers/express";
+import { runSocket } from "./servers/socket";
 
 const PORT = process.env.PORT || "3001";
 
@@ -21,48 +16,5 @@ const io = new socketIO.Server(httpServer, {
 	},
 });
 
-// Configure express
-app.use(express.json()); // Enable json format
-app.use(express.static(path.join(__dirname, "../build")));
-app.use(
-	cors({
-		origin: ["http://localhost:3000", "https://chat-frithjof.herokuapp.com"],
-	})
-);
-
-// Add express routes
-app.use("/api/user", UserRouter); // User route
-app.get("/*", (req, res) => {
-	res.sendFile(path.join(__dirname, "../build", "index.html"));
-});
-
-db.sequelize
-	.sync()
-	.then(() => {
-		httpServer.listen(PORT, (): void => {
-			console.log(`Server running on port ${PORT}...`);
-		});
-	})
-	.catch((err: any) => {
-		console.log(err);
-	});
-
-// Run express & socket servers
-io.on("connection", (socket) => {
-	console.log("Socket: ", socket.id);
-
-	socket.on("join_room", (room: string) => {
-		socket.join(room);
-		console.log(`User [${socket.id}] joined ${room}`);
-	});
-
-	socket.on("send_message", (payload: any) => {
-		socket.to(payload.room).emit("receive_message", payload.message);
-	});
-
-	socket.on("disconnect", () => {
-		console.log("User disconnected...");
-	});
-});
-// runExpress(app, httpServer, PORT);
-// runSocket(io);
+runExpress(app, httpServer, PORT);
+runSocket(io);
